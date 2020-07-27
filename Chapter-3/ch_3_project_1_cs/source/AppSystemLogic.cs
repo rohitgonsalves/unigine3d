b36a7d516d9fb1eb6ac9318bb205265aedc295b5
Rohit Gonsalves <rohitgonsalves@outlook.com>
2020-07-27 14:36:12 +0530

    Chapter 3 C# and CPP Project. Flower Texture Resource. .gitignore Update for Chapter 3.

---
+++ Chapter-3/ch_3_project_1_cs/source/AppSystemLogic.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Unigine;
+
+namespace UnigineApp
+{
+	class AppSystemLogic : SystemLogic
+	{
+		// System logic, it exists during the application life cycle.
+		// These methods are called right after corresponding system script's (UnigineScript) methods.
+
+		public AppSystemLogic()
+		{
+		}
+
+		public override bool Init()
+		{
+			// Write here code to be called on engine initialization.
+			World.LoadWorld("ch_3_project_1_cs");
+			vec3 vector = new vec3(5.0f, 5.0f, 5.0f);
+			vector.Normalize();
+			float x = vector.x;
+			float y = vector.y;
+			float z = vector.z;
+			float len = vector.Length;
+
+			return true;
+		}
+
+		// start of the main loop
+		public override bool Update()
+		{
+			// Write here code to be called before updating each render frame.
+
+			return true;
+		}
+
+		public override bool PostUpdate()
+		{
+			// Write here code to be called after updating each render frame.
+
+			return true;
+		}
+		// end of the main loop
+
+		public override bool Shutdown()
+		{
+			// Write here code to be called on engine shutdown.
+
+			return true;
+		}
+	}
+}
